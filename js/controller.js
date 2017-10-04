f465741c82007096647a41b7522cbbac7a67742d
Slasten Tikhon <slasten32@gmail.com>
2017-10-04 23:17:02 +1000

    Draw chart on button click

--- js/controller.js
+++ js/controller.js
@@ -14,3 +14,37 @@ function genXS(a, b, n) {
 
     return xs;
 }
+
+google.charts.load('current', {'packages':['corechart']});
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['X', 'Эйлера', 'Модифицированный Эйлера', 'Рунге-Кутты 4-го порядка'],
+      [0, 0, 1.5, -1.5],
+      [5, 20, 21.5, 18.5],
+      [10, 10, 11.5, 8.5],
+      [20, 0, 1.5, -1.5]
+    ]);
+
+    var options = {
+        title: 'График',
+        curveType: 'line',
+        legend: { position: 'bottom' },
+        explorer: {
+            actions: ['dragToZoom', 'rightClickToReset'],
+            axis: 'horizontal',
+            keepInBounds: true,
+            maxZoomIn: 10.0
+        }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+    chart.draw(data, options);
+}
+
+document.getElementById('apply').onclick = () => {
+    drawChart();   
+};
+
+window.onresize = drawChart;

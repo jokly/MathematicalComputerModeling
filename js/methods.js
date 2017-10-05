ed5b700670f647c97cfe570ec9887039efa7f6bc
Slasten Tikhon <slasten32@gmail.com>
2017-10-06 00:56:03 +1000

    Fix methods

--- js/methods.js
+++ js/methods.js
@@ -16,7 +16,7 @@ export function euler(a, b, xs, y0, f) {
     let h = (b - a) / n;
     let e = (y_prev, x_prev) => { return y_prev + h * f(x_prev, y_prev) };
 
-    let ys = [e(y0, a)];
+    let ys = [y0];
     for (let i = 1; i < n; i++) {
         ys.push(e(ys[i - 1], xs[i - 1]));
     }
@@ -29,8 +29,8 @@ export function euler_up(a, b, xs, y0, f) {
     let h = (b - a) / n;
     let e = (y_prev, x_prev, x, y_corrector) => { return y_prev + h * (f(x_prev, y_prev) + f(x, y_corrector)) / 2 };
 
-    let ys = euler(a, b, xs, y0, f);
-    let ys_up = [e(y0, a, a + h, ys[1])];
+    let ys = euler(a, b, xs, y0, f).ys;
+    let ys_up = [y0];
 
     for (let i = 1; i < n; i++) {
         ys_up.push(e(ys_up[i - 1], xs[i - 1], xs[i], ys[i]));

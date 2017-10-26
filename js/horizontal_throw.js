c52f86c4445cfad1fa13f4b3e16969b49215fe5f
Tikhon Slasten <slasten32@gmail.com>
2017-10-26 23:23:49 +1000

    Add new process modeling

---
+++ js/horizontal_throw.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    document.getElementById('chart-card').style.display = 'none';
+    document.getElementById('table-card').style.display = 'none';
+
+    $('select').material_select();
+};
